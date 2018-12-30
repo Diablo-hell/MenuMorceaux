0d43fbe10edf311775f7c4c955f5432a486edf28
Remi_V <akahaka56@gmail.com>
2018-12-31 00:54:37 +0100

    mise au propre
    ajout de la génération auto des jours de la semaine
    styles flex
    tailles qui s'ajustent (enfin), et qui ne sont pas fixées

--- MenuMorceaux.js
+++ MenuMorceaux.js
@@ -1,5 +1,5 @@
 
-var calendrier=[];
+var calendrier=[];	
 
 calendrier["01-11-18"] = {};
 
@@ -7,5 +7,42 @@ var modeleSemaine = {};
 modeleSemaine.dateLundi="181030";
 modeleSemaine.repas=[];
 
-calendrier["01-11-18"] = modeleSemaine;
+calendrier["01-11-18"] = modeleSemaine;	
 console.log("calendrier test : " + calendrier["01-11-18"].dateLundi);
+
+jours = ["Lundi","Mardi","Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+$("#titreMenu").show();
+$("#titrePlats").hide();
+$("#titreCourses").hide();
+
+for(u=0;u<7;u++)
+{
+	$("#TabMenuSemaine").append(''+
+						'<div id="Col' + jours[u] + '" class="ccc ColJour">'+
+							'<div class="jour ccc">' + jours[u] + '</div>'+
+							'<div class="ConteneurMidiSoir">'+
+								'<div class="MidiSoir">Midi</div>'+
+							'</div>'+
+							'<div class="ConteneurMidiSoir">'+
+								'<div class="MidiSoir">Soir</div>'+
+							'</div>'+
+							'<div class="notes"><p>notes</p></div>'+
+						'</div>');
+}
+/* Generation des classes flex */
+
+/*
+dir=[["r","c"],["row","column"]];
+justify=[["s","e","c","b","a","v"],["start","end","center","space-between","space-around","space-evenly"]];
+align=[["s","e","c","t"],["start","end","center","stretch"]];
+for(u=0;u<2;u++)
+	for(v=0;v<6;v++)
+		for(w=0;w<4;w++)
+		{
+			console.log("."+dir[0][u]+justify[0][v]+align[0][w]+"{"+
+							"\tflex-direction: " + dir[1][u]+";"+
+							"\tjustify-content: " + justify[1][v]+";"+
+							"\talign-items: " + align[1][w]+";"+
+							"}");
+		}*/
