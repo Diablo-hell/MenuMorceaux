152ba59e0eb86e24dbbf3694d3ed313ddea7b0c8
Remi_V <akahaka56@gmail.com>
2019-01-02 22:28:40 +0100

    1.04
    ajout de callbacks click défaut, plats favoris et midi/soir

--- MenuMorceaux.js
+++ MenuMorceaux.js
@@ -12,9 +12,10 @@ console.log("calendrier test : " + calendrier["01-11-18"].dateLundi);
 
 jours = ["Lundi","Mardi","Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 
-$("#titreMenu").show();
-$("#titrePlats").hide();
-$("#titreCourses").hide();
+$("#headBar #titreMenu").show();
+$("#headBar #titrePlats").hide();
+$("#headBar #titreCourses").hide();
+$("#headBar #mainQuittButton").hide();
 
 for(u=0;u<7;u++)
 {
@@ -23,13 +24,56 @@ for(u=0;u<7;u++)
 							'<div class="jour ccc">' + jours[u] + '</div>'+
 							'<div class="ConteneurMidiSoir">'+
 								'<div class="MidiSoir">Midi</div>'+
+								'<div class="repasMidiSoir"></div>'+
 							'</div>'+
 							'<div class="ConteneurMidiSoir">'+
 								'<div class="MidiSoir">Soir</div>'+
+								'<div class="repasMidiSoir"></div>'+
 							'</div>'+
 							'<div class="notes"><p>notes</p></div>'+
 						'</div>');
 }
+
+last_select=null
+$("*").click(function(evt){
+	evt.stopPropagation();
+	// un-select last selected object
+	$(last_select).css("border-width","");
+	$(last_select).css("border-color","");
+	$(last_select).css("border-style","");
+	$(last_select).css("border-radius","");
+	console.log("default click vu");
+	last_select=null
+	
+})
+
+/* callback click dans les cases conteneurMidiSoir */
+$(".ConteneurMidiSoir").prop("onclick", null).off("click");
+$(".ConteneurMidiSoir *").prop("onclick", null).off("click");
+$(".ConteneurMidiSoir").click(function(evt){
+	evt.stopPropagation();
+	$(last_select).css("border-width","");
+	$(last_select).css("border-color","");
+	$(last_select).css("border-style","");
+	$(last_select).css("border-radius","");
+	
+	$(this).css("border-width","3px");
+	$(this).css("border-color","blue");
+	$(this).css("border-style","solid");
+	$(this).css("border-radius","8px");
+	
+	console.log("click vu");
+	last_select=this;
+})
+
+
+/* callback click dans les cases conteneurMidiSoir */
+$("#boutonFavoris #platsFavorisBtn").prop("onclick", null).off("click");
+$("#boutonFavoris #platsFavorisBtn *").prop("onclick", null).off("click");
+$("#boutonFavoris #platsFavorisBtn").click(function(evt){
+	$(last_select).find(".repasMidiSoir").append("gratin de limaces");
+})
+
 /* Generation des classes flex */
 
 /*
